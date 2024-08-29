7947ae5d15189a40f12bcbf0c2e5cd37ec8239b4
FSFahim <farhanshahriar3728@gmail.com>
2024-08-29 17:23:13 +0600

    Added Router and Set the Background

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,57 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import AppLayout from "./layouts/AppLayout";
+import LandingPage from "./pages/LandingPage";
+import Onboarding from "./pages/Onboarding";
+import JobListing from "./pages/JobListing";
+import Job from "./pages/Job";
+import PostJob from "./pages/PostJob";
+import SavedJobs from "./pages/SavedJobs";
+import MyJobs from "./pages/MyJobs";
+import { ThemeProvider } from "./components/theme-provider";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/onboarding",
+        element: <Onboarding />,
+      },
+      {
+        path: "/jobs",
+        element: <JobListing />,
+      },
+      {
+        path: "/job/:id",
+        element: <Job />,
+      },
+      {
+        path: "/post-job",
+        element: <PostJob />,
+      },
+      {
+        path: "/saved-jobs",
+        element: <SavedJobs />,
+      },
+      {
+        path: "/my-jobs",
+        element: <MyJobs />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <div>Welcome to HireHaven</div>;
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
